bbe64dac445cfbe435cce372f739069a7e977243
quan3473 <nguyenminhquan042004@gmail.com>
2025-01-16 01:49:23 +0700

    .

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -4,7 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/common/i18n/request.ts');
 
 const sassOptions = {
-  additionalData: `
+    additionalData: `
     $var: red;
   `,
 }
@@ -20,40 +20,44 @@ const experimental = {
     }
 }
 
-const compiler = { }
+const compiler = {}
 
-const onDemandEntries = { }
+const onDemandEntries = {}
 
 const nextConfig: NextConfig = {
-  cleanDistDir: true,
-  sassOptions: {
-    ...sassOptions,
-    implementation: 'sass-embedded',
-  },
-  compiler: {
-    ...compiler,
-    styledComponents: true,
-  },
-  productionBrowserSourceMaps: true,
-  onDemandEntries: {
-    ...onDemandEntries,
-    maxInactiveAge: 30 * 1000, 
-    pagesBufferLength: 5, 
-  },
-  experimental: {
-    ...experimental,
-    authInterrupts: true,
-    inlineCss: true,
-    optimizeCss: true,
-    optimizeServerReact: true,
-    scrollRestoration: true,
-    nextScriptWorkers: true,
-    parallelServerCompiles: true,
-    parallelServerBuildTraces: true,
-    webpackBuildWorker: true,
-    webpackMemoryOptimizations: true
-  },
-//   output: 'export'
+    cleanDistDir: true,
+    sassOptions: {
+        ...sassOptions,
+        implementation: 'sass-embedded',
+    },
+    compiler: {
+        ...compiler,
+        styledComponents: true,
+    },
+    productionBrowserSourceMaps: true,
+    onDemandEntries: {
+        ...onDemandEntries,
+        maxInactiveAge: 30 * 1000,
+        pagesBufferLength: 5,
+    },
+    experimental: {
+        ...experimental,
+        authInterrupts: true,
+        inlineCss: true,
+        optimizeCss: true,
+        optimizeServerReact: true,
+        scrollRestoration: true,
+        nextScriptWorkers: true,
+        parallelServerCompiles: true,
+        parallelServerBuildTraces: true,
+        webpackBuildWorker: true,
+        webpackMemoryOptimizations: true
+    },
+    //   output: 'export'
+    // i18n: {
+    //     locales: ['en', 'vi'],
+    //     defaultLocale: 'en',
+    // },
 }
 
 export default withNextIntl(nextConfig);
